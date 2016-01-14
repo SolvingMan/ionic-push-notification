cbb311bf852dd0ffb3be3562de3ad145d2bc3a40
Jiri Spac <capajj@gmail.com>
2016-01-15 00:22:13 +0100

    converted to standard code style

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-// Dependencies
-var http = require('http');
-var querystring = require('querystring');
+'use strict'
+const http = require('http')
+const log = require('debug')('ionic-push-notification')
 
-module.exports = function (credentials, notification){
-  return new Promise(function(resolve, reject){
-
-    // Serialize notification into a query string. See docs @ https://nodejs.org/api/querystring.html#querystring_querystring_stringify_obj_sep_eq_options, Node.js v0.12.1
-    var postData = querystring.stringify(notification);
-
+module.exports = function (credentials, notification) {
+  return new Promise(function (resolve, reject) {
     // Object that defines http.request(). See docs @ https://nodejs.org/api/http.html#http_http_request_options_callback, Node.js v0.12.1
     var options = {
       hostname: 'push.ionic.io',
@@ -15,35 +11,35 @@ module.exports = function (credentials, notification){
       path: '/api/v1/push',
       method: 'POST',
       headers: {
-        "Authorization": "Basic " + new Buffer(credentials.IonicApplicationAPIsecret + ":").toString("base64"),
-        "Content-Type" : "application/json",
-        "X-Ionic-Application-Id" : credentials.IonicApplicationID
+        'Authorization': 'Basic ' + new Buffer(credentials.IonicApplicationAPIsecret + ':').toString('base64'),
+        'Content-Type': 'application/json',
+        'X-Ionic-Application-Id': credentials.IonicApplicationID
       }
-    };
+    }
 
   // HTTP POST request. See docs @ https://nodejs.org/api/http.html#http_http_request_options_callback, Node.js v0.12.1
-    var req = http.request(options, function(res) {
-      console.log('STATUS: ' + res.statusCode);
-      console.log('HEADERS: ' + JSON.stringify(res.headers));
-      res.setEncoding('utf8');
-      var responseBody = '';
+    var req = http.request(options, function (res) {
+      log('STATUS: ' + res.statusCode)
+      log('HEADERS: ' + JSON.stringify(res.headers))
+      res.setEncoding('utf8')
+      var responseBody = ''
       res.on('data', function (chunk) {
-        console.log('BODY: ' + chunk);
-        responseBody = responseBody + chunk;
-      });
+        log('BODY: ' + chunk)
+        responseBody = responseBody + chunk
+      })
       res.on('end', function () {
-        resolve(JSON.parse(responseBody));
-      });
-    });
+        resolve(JSON.parse(responseBody))
+      })
+    })
 
     // Error handling.
-    req.on('error', function(e) {
-      console.log('problem with request: ' + e.message);
-      reject({"result":"error","message":e.message});  
-    });
+    req.on('error', function (e) {
+      log('problem with request: ' + e.message)
+      reject(e)
+    })
 
     // Wite data to request body
-    req.write(JSON.stringify(notification));
-    req.end();
-  });
-};
+    req.write(JSON.stringify(notification))
+    req.end()
+  })
+}
